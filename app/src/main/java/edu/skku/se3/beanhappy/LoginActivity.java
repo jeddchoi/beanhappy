326bd6efb7c7a825092d23e01c02a265433c560c
Jed <luv.gwangyoung@gmail.com>
2018-05-22 16:43:57 +0900

    mainActivity

--- app/src/main/java/edu/skku/se3/beanhappy/LoginActivity.java
+++ app/src/main/java/edu/skku/se3/beanhappy/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.skku.se3.beanhappy;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,9 +29,12 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -38,6 +42,8 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+    TextView show_agreement;
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -92,7 +98,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.email_login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        show_agreement = findViewById(R.id.show_agreementText);
+
+        show_agreement.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), R.string.agreement, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
@@ -184,17 +200,32 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
         }
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
+
+//        String regex = "^[_a-zA-Z0-9-\\.]+@skku.edu";
+//        Pattern p = Pattern.compile(regex);
+//        Matcher m = p.matcher(email);
+//        boolean isNormal = m.matches();
+//
+//        return isNormal;
+        return true;
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
+
+//        String regex = "^[a-zA-Z0-9!@.#$%^&*?_~]{8,16}$"; // 8자리 ~ 16자리까지 가능
+//        Pattern p = Pattern.compile(regex);
+//        Matcher m = p.matcher(password);
+//        boolean isNormal = m.matches();
+//
+//        return isNormal;
+        return true;
     }
 
     /**
