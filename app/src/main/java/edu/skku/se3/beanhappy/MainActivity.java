1afe7995db1552176a30bf0d76e3aa5361ff0e63
Minki-kim95 <rlaalsrl7777@gmail.com>
2018-05-24 17:57:34 +0900

    Merge branch 'master' of https://github.com/jeddchoi/beanhappy into minkikim
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml
    
    make using activity and try to connect main(suc)

--- app/src/main/java/edu/skku/se3/beanhappy/MainActivity.java
+++ app/src/main/java/edu/skku/se3/beanhappy/MainActivity.java
@@ -1,13 +1,25 @@
 package edu.skku.se3.beanhappy;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Context mContext = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.button4);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentToUsing = new Intent(mContext, usingactivity.class);
+                startActivity(intentToUsing);
+            }
+        });
     }
 }
