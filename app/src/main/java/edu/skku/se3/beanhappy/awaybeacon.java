c8dab8560881d0518a5421bbbd71a0a9baa3f49d
minki_KIM <rlaalsrl7777@gmail.com>
2018-05-25 19:23:17 +0900

    fragment 삭제 activity생성

---
+++ app/src/main/java/edu/skku/se3/beanhappy/awaybeacon.java
@@ -0,0 +1,13 @@
+package edu.skku.se3.beanhappy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class awaybeacon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_awaybeacon);
+    }
+}
