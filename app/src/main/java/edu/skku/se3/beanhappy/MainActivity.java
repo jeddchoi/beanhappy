a3c863424106489ddd8a11cba1d55ff4dfc3442a
jeddchoi <luv.gwangyoung@gmail.com>
2018-05-30 02:53:14 +0900

    now working

--- app/src/main/java/edu/skku/se3/beanhappy/MainActivity.java
+++ app/src/main/java/edu/skku/se3/beanhappy/MainActivity.java
@@ -1,46 +1,89 @@
 package edu.skku.se3.beanhappy;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements
+        View.OnClickListener {
     public static final String TAG = "BeanHappy";
     private FirebaseAuth mAuth;
+    TextView logout_textBtn;
+    Switch pushAlarmSwitch;
+    Button quickReserveBtn, reserveBtn, myStatusBtn, reportBtn;
+    TextView seatNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // top ui
+        logout_textBtn = (TextView)findViewById(R.id.logout_textBtn);
+        pushAlarmSwitch = (Switch)findViewById(R.id.pushAlarmSwitch);
+        seatNum = (TextView)findViewById(R.id.seatNum);
+
+        // Buttons
+        quickReserveBtn = (Button)findViewById(R.id.quickReserveBtn);
+        reserveBtn = (Button)findViewById(R.id.reserveBtn);
+        myStatusBtn = (Button)findViewById(R.id.myStatusBtn);
+        reportBtn = (Button)findViewById(R.id.reportBtn);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        Log.d(TAG, "signIn:" + currentUser.getEmail().toString());
-      
-        Button btn = (Button)findViewById(R.id.button4);
-        Button chat_btn = (Button)findViewById(R.id.button5);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(checkusingbeanbag.using_beanbag == 0) {
-                    Intent intentToUsing = new Intent(mContext, usingactivity.class);
-                    startActivity(intentToUsing);
-                    checkusingbeanbag.using_beanbag = 1;
-                }
-                finish();
+        Log.d(TAG, "signIn:" + currentUser.getEmail().toString()); // test
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.quickReserveBtn) {
+
+        } else if (i == R.id.reserveBtn) {
+
+        } else if (i == R.id.myStatusBtn) {
+            if(checkusingbeanbag.using_beanbag == 0) {
+                Intent intentToUsing = new Intent(getApplicationContext(), usingactivity.class);
+                startActivity(intentToUsing);
+                checkusingbeanbag.using_beanbag = 1;
             }
-        });
-        chat_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentToChat = new Intent(mContext, ChatActivity.class);
-                startActivity(intentToChat);
-            }
-        });
+            finish();
+        } else if (i == R.id.reportBtn) {
+            Intent intentToChat = new Intent(getApplicationContext(), ChatActivity.class);
+            startActivity(intentToChat);
+        } else if (i == R.id.logout_textBtn) {
+
+        }
     }
 }
