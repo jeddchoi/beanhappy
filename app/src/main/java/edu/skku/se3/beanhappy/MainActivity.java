50b96f4d94f971effac1eea20729f8a86462740f
Jed <luv.gwangyoung@gmail.com>
2018-05-30 01:52:31 +0900

    login, register, email-verification, firebase, drawables

--- app/src/main/java/edu/skku/se3/beanhappy/MainActivity.java
+++ app/src/main/java/edu/skku/se3/beanhappy/MainActivity.java
@@ -2,13 +2,25 @@ package edu.skku.se3.beanhappy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "BeanHappy";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        Log.d(TAG, "signIn:" + currentUser.getEmail().toString());
 
     }
 }
