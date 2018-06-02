e48aa4ed14b8647810ce60fff05743e7cb5f89e3
rladbsgh7586 <rladbsgh7586@naver.com>
2018-06-02 14:18:27 +0900

    [edit] 채팅기능 구현(오픈채팅가능)

--- app/src/main/java/edu/skku/se3/beanhappy/ChatActivity.java
+++ app/src/main/java/edu/skku/se3/beanhappy/ChatActivity.java
@@ -30,7 +30,7 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chat);
         initViews();
         initFirebaseDatabase();
         initValues();
