6697cc79879c122ef1c3994cb056edccebb4ca7b
eujin <kstar1996@gmail.com>
2018-06-09 18:46:08 +0900

    layout changes..

--- app/src/main/java/edu/skku/se3/beanhappy/BeaconReferenceApplication.java
+++ app/src/main/java/edu/skku/se3/beanhappy/BeaconReferenceApplication.java
@@ -10,11 +10,14 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 
+//Done
 
 public class BeaconReferenceApplication extends Application implements BootstrapNotifier {
     private static final String TAG = "BeaconReferenceApp";
@@ -28,24 +31,20 @@ public class BeaconReferenceApplication extends Application implements Bootstrap
         super.onCreate();
         BeaconManager beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
 
-        // By default the AndroidBeaconLibrary will only find AltBeacons.  If you wish to make it
-        // find a different type of beacon, you must specify the byte layout for that beacon's
-        // advertisement with a line like below.  The example shows how to find a beacon with the
-        // same byte layout as AltBeacon but with a beaconTypeCode of 0xaabb.  To find the proper
-        // layout expression for other beacon types, do a web search for "setBeaconLayout"
-        // including the quotes.
-        //
-        //beaconManager.getBeaconParsers().clear();
-        //beaconManager.getBeaconParsers().add(new BeaconParser().
-        //        setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        // It will find AltBeacons.
+        // AltBeacon을 찾습니다
+
+        beaconManager.getBeaconParsers().clear();
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
 
         Log.d(TAG, "setting up background monitoring for beacons and power saving");
+
         // wake up the app when a beacon is seen
         Region region = new Region("backgroundRegion",
-                null, null, null);
+                Identifier.parse("77a29b0c-81a7-44a1-b8dc-763c28003082"), null, null);
         regionBootstrap = new RegionBootstrap(this, region);
 
-        // simply constructing this class and holding a reference to it in your custom Application
         // class will automatically cause the BeaconLibrary to save battery whenever the application
         // is not visible.  This reduces bluetooth power usage by about 60%
         backgroundPowerSaver = new BackgroundPowerSaver(this);
@@ -57,24 +56,28 @@ public class BeaconReferenceApplication extends Application implements Bootstrap
         //***************************
     }
 
+
     @Override
+    //Called when at least one beacon in a Region is visible.
     public void didEnterRegion(Region arg0) {
+        // 위에 나온 region 에서 비콘이 detect 되면 did enter region 이라는 문구가 뜬다
         // In this example, this class sends a notification to the user whenever a Beacon
         // matching a Region (defined above) are first seen.
         Log.d(TAG, "did enter region.");
         if (!haveDetectedBeaconsSinceBoot) {
             Log.d(TAG, "auto launching MainActivity");
 
-            // The very first time since boot that we detect an beacon, we launch the
-            // MainActivity
+            // The very first time since boot that we detect an beacon, we launch the MainActivity
+
             Intent intent = new Intent(this, MonitoringActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            // Important:  make sure to add android:launchMode="singleInstance" in the manifest
-            // to keep multiple copies of this activity from getting created if the user has
+
             // already manually launched the app.
             this.startActivity(intent);
             haveDetectedBeaconsSinceBoot = true;
-        } else {
+        }
+
+        /*else {
             if (monitoringActivity != null) {
                 // If the Monitoring Activity is visible, we log info about the beacons we have
                 // seen on its display
@@ -84,13 +87,20 @@ public class BeaconReferenceApplication extends Application implements Bootstrap
                 // the foreground, we send a notification to the user on subsequent detections.
                 Log.d(TAG, "Sending notification.");
                 sendNotification();
+
             }
         }
+        */
+        //위에 코드는 비콘을 못 dete
 
 
     }
 
+
+
     @Override
+    //비콘이 region 벗어남
+    //Called when no beacons in a Region are visible.
     public void didExitRegion(Region region) {
         if (monitoringActivity != null) {
             monitoringActivity.logToDisplay("I no longer see a beacon.");
@@ -98,6 +108,7 @@ public class BeaconReferenceApplication extends Application implements Bootstrap
     }
 
     @Override
+    //Called with a state value of MonitorNotifier.INSIDE when at least one beacon in a Region is visible.
     public void didDetermineStateForRegion(int state, Region region) {
         if (monitoringActivity != null) {
             monitoringActivity.logToDisplay("I have just switched from seeing/not seeing beacons: " + state);
--- app/src/main/java/edu/skku/se3/beanhappy/MainActivity.java
+++ app/src/main/java/edu/skku/se3/beanhappy/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -44,6 +45,8 @@ public class MainActivity extends BaseActivity implements
     private DeviceUuidFactory device;
     private User CurrentUser;
     private FirebaseUser user;
+
+
 
 
     @Override
